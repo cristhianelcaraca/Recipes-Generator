166a3a639d3312021ade89ffd10dffe8c33a47c3
cristhianelcaraca <cristhianelcaraca@gmail.com>
2026-03-12 20:32:17 +0000

    integrated JS functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Devil's food cake",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
